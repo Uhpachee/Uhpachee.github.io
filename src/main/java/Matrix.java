16b8a257dd8baf92dad9d3ef2a5dd19b76ce1757
Uhpachee <sea7wa@gmail.com>
2022-03-14 08:33:16 -0700

    #3

--- src/main/java/Matrix.java
+++ src/main/java/Matrix.java
@@ -1,5 +1,5 @@
-import java.util.Arrays;
 
+// matrix class is used to store and format the output of a matrix
 public class Matrix {
     private final int[][] matrix;
 
@@ -9,12 +9,31 @@ public class Matrix {
     }
 
     // Hack: create toString method using nested for loops to format output of a matrix
-    public String toString(Matrix matrix) {
-        String swapped;/*
-        this.matrix
-        for (this.matrix.length())
-        return swapped;*/
-        return "adsf";
+    public String toString() {
+        String output = "";
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == -1) {
+                    output = output + "  ";
+                } else {
+                    output = output +  matrix[i][j] + " ";
+                }
+            }
+            output += "\n";
+        }
+
+        for (int i = matrix.length - 1; i >= 0; i--) {
+            output += "\n";
+            for (int j = matrix[i].length - 1; j >= 0; j--) {
+                if (matrix[i][j] == -1) {
+                    output = output + "  ";
+                } else {
+                    output = output +  matrix[i][j] + " ";
+                }
+            }
+        }
+
+        return output;
     }
 
     // declare and initialize a matrix for a keypad
@@ -33,11 +52,11 @@ public class Matrix {
     public static void main(String[] args) {
         Matrix m0 = new Matrix(keypad());
         System.out.println("Keypad:");
-        System.out.println(Arrays.deepToString(m0.matrix));
+        System.out.println(m0);
 
         Matrix m1 = new Matrix(numbers());
         System.out.println("Numbers Systems:");
-        System.out.println(Arrays.deepToString(m1.matrix));
+        System.out.println(m1);
     }
 
 }
